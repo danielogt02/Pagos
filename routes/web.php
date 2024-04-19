7a8400519bafd7d99b050c9b75f0391fe2900db5
Daniel Ortiz <danielog.dev@gmail.com>
2024-04-18 19:09:49 -0600

    creacion de vistas

--- routes/web.php
+++ routes/web.php
@@ -1,22 +1,24 @@
 <?php
 
-use App\Http\Controllers\CursoController;
-use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\homeController;
+    use App\Http\Controllers\CursoController;
+    use Illuminate\Support\Facades\Route;
+    use App\Http\Controllers\homeController;
 
+    Route::get('/', HomeController::class);
 
-Route::get('/', HomeController::class);
+    Route::get('cursos', [CursoController::class, 'index']);
+    route::get('cursos/create',[CursoController::class, 'create']);
+    Route::get('cursos/{curso}', [CursoController::class, 'show']);
 
-Route::get('cursos', [CursoController::class, 'index']);
-
-route::get('cursos/create',[CursoController::class, 'create']);
-
-Route::get('cursos/{curso}', [CursoController::class, 'show']);
-
-/*Route::get('cursos/{curso}/{categoria?}',function($curso,$categoria=null){
-    if($categoria){
-        return "Bienvenido al curso $curso de la categoria de $categoria";
-    }else{
-        return "Bienvenido al curso $curso";
-    }
-});*/
+    /*Route::controller(CursoController::class)->group(function(){
+        Route::get('cursos', 'index');
+        route::get('cursos/create','create');
+        Route::get('cursos/{curso}','show');
+    });*/
+    /*Route::get('cursos/{curso}/{categoria?}',function($curso,$categoria=null){
+        if($categoria){
+            return "Bienvenido al curso $curso de la categoria de $categoria";
+        }else{
+            return "Bienvenido al curso $curso";
+        }
+    });*/
