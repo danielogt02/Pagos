cabaaa30fa0d68b60b208af6f56c94d2be12ef89
Daniel Ortiz <danielog.dev@gmail.com>
2024-04-19 17:45:46 -0600

    migraciones, seeders, factories

--- database/migrations/2024_04_19_020310_create_cursos_table.php
+++ database/migrations/2024_04_19_020310_create_cursos_table.php
@@ -14,7 +14,8 @@ return new class extends Migration
         Schema::create('cursos', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->text('description');
+            $table->text('descripcion');
+            $table->text('categoria');
             $table->timestamps();
         });
     }
